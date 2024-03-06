0a122056e5b05726b497cb68d718728b22231e6b
AnhKhoa <nguyenanhkhoa1699@gmail.com>
2024-03-06 19:34:18 +0700

    them Product owner

---
+++ WebAppRazorpage/Pages/ProductOwner/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppRazorpage.Pages.ProductOwner
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebAppRazorpage/Pages/ProductOwner/Profile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppRazorpage.Pages.ProductOwner
+{
+    public class ProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
