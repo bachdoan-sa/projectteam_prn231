b2f6ba83c88060befdb376695c46e4af0c71d2ee
Dangkhoa305 <dangkhoa1699@gmail.com>
2024-03-06 21:26:16 +0700

    sửa trang tạo đơn đấu giá

--- WebAppRazorpage/Pages/ProductOwner/CreateAuctionEvent.cshtml.cs
+++ WebAppRazorpage/Pages/ProductOwner/CreateAuctionEvent.cshtml.cs
@@ -5,8 +5,29 @@ namespace WebAppRazorpage.Pages.ProductOwner
 {
     public class CreateAuctionEventModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public double? StartingPrice { get; set; }
+
+        [BindProperty]
+        public double? ExpectedPrice { get; set; }
+
+        [BindProperty]
+        public double? MinRaise { get; set; }
+
+        [BindProperty]
+        public double? MaxRaise { get; set; }
+
+        [BindProperty]
+        public DateTime? BeginDateTime { get; set; }
+
+        [BindProperty]
+        public DateTime? EndDateTime { get; set; }
+
+        [BindProperty]
+        public double? FinalPrice { get; set; }
+        public void OnPost()
         {
+
         }
     }
 }
