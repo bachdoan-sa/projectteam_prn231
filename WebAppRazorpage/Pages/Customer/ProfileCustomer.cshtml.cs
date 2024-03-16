1ca39e1efdc6e1abeda1a9926c6fb71166c5095d
AnhKhoa <nguyenanhkhoa1699@gmail.com>
2024-03-16 22:12:20 +0700

    Customer profile

--- WebAppRazorpage/Pages/Customer/ProfileCustomer.cshtml.cs
+++ WebAppRazorpage/Pages/Customer/ProfileCustomer.cshtml.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Http.Headers;
+using WebAppRazorpage.ApiModel;
+using WebAppRazorpage.Constants;
 
 namespace WebAppRazorpage.Pages.Customer
 {
     public class ProfileCustomerModel : PageModel
     {
-        public void OnGet()
+
+    private readonly HttpClient _client = new HttpClient();
+        [BindProperty]
+
+        public AccountModel? Customer { get; set; }
+
+
+        public IActionResult OnGet(int customerId)
         {
+            //var accessToken = HttpContext.Session.GetString("JwToken");
+            //_client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            HttpResponseMessage result = _client.GetAsync($"https://localhost:7253/api/Account/get-single/{customerId}").Result;
+        
+            if (result.IsSuccessStatusCode)
+            {
+                Task<string> readString = result.Content.ReadAsStringAsync();
+                string jsonString=  readString .Result;
+                Customer = AccountModel.FromJson(jsonString).FirstOrDefault();
+            }
+            if(Customer == null)
+            {
+                return Redirect("/SignIn");
+            }
+            return Page();
+        
         }
     }
 }
