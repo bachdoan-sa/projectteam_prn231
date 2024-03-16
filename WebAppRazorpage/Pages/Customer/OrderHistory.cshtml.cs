a6aa2bd10864fb03ac1eca34188e239061d36158
Dangkhoa305 <dangkhoa1699@gmail.com>
2024-03-16 15:48:15 +0700

    trang orderHistory

--- WebAppRazorpage/Pages/Customer/OrderHistory.cshtml.cs
+++ WebAppRazorpage/Pages/Customer/OrderHistory.cshtml.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebAppRazorpage.ApiModel;
+using WebAppRazorpage.Constants;
 
 namespace WebAppRazorpage.Pages.Customer
 {
     public class OrderHistoryModel : PageModel
     {
-        public void OnGet()
+        private readonly HttpClient client = new HttpClient();
+
+
+        public List<OrderModel> ListOrder { get; set; }
+
+        public void OnGet(int customerId)
         {
+            var task = client.GetAsync($"https://localhost:7253/api/Order/Customer{customerId}");
+            HttpResponseMessage result = task.Result;
+            List<OrderModel> listOrder = new List<OrderModel>();
+            if (result.IsSuccessStatusCode)
+            {
+                Task<string> readString = result.Content.ReadAsStringAsync();
+                string jsonString = readString.Result;
+                listOrder = OrderModel.FromJson(jsonString);
+            }
+            ListOrder = listOrder;
         }
     }
 }
