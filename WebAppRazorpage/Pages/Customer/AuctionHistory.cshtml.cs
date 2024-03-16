dbbc86752bdb4ef09b01acf176e047750901b8b9
Dangkhoa305 <dangkhoa1699@gmail.com>
2024-03-16 14:13:04 +0700

    thêm 3 trang customer

---
+++ WebAppRazorpage/Pages/Customer/AuctionHistory.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppRazorpage.Pages.Customer
+{
+    public class AuctionHistoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
